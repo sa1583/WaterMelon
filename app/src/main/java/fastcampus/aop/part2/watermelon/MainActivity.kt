6273cac50c2a40c368493f89b5cec2088333ea12
sa1583 <sa1583@naver.com>
2022-04-05 14:58:29 +0900

    ADD: 재생화면 UI

--- app/src/main/java/fastcampus/aop/part2/watermelon/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/watermelon/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment.newInstance())
+            .commit()
     }
 }
---
+++ app/src/main/java/fastcampus/aop/part2/watermelon/PlayerFragment.kt
@@ -0,0 +1,17 @@
+package fastcampus.aop.part2.watermelon
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance(): PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+}
